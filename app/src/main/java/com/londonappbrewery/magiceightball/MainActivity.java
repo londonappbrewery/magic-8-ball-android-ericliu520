5e0e61547252bc09bad18ad21a6da6857e3225c7
ERIC <ericliu520@gmail.com>
2020-09-13 13:08:57 +1000

    MY 8 BALL GAME IS DONE

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,12 +2,37 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    
+    private ImageView imageViewBall;
+    private Button askButton;
+    private int myNumber;
+    private int[] myBall;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myBall = new int[] {R.drawable.ball1,R.drawable.ball2,R.drawable.ball3,R.drawable.ball4,R.drawable.ball5,};
+        myNumber = new Random().nextInt(5);
+        imageViewBall = (ImageView) findViewById(R.id.imageViewBall);
+        askButton = (Button) findViewById(R.id.askButton);
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(MainActivity.this, "Ask Me", Toast.LENGTH_SHORT).show();
+                imageViewBall.setImageResource(myBall[myNumber]);
+                myNumber = new Random().nextInt(5);
+            }
+        });
+
     }
 }
